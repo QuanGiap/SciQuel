ec84284fc56ac0f76c3e18464f25ae2a0bdede1c
QuanGiap <minhquangiapnguyen@gmail.com>
2024-05-15 16:36:31 -0700

    Create ztype schema for subpart

--- src/app/api/quizzes/schema.ts
+++ src/app/api/quizzes/schema.ts
@@ -1,7 +1,101 @@
 import { z } from "zod";
 
+export const complexMatchingSubpartSchema = z
+  .object({
+    question: z.string(),
+    categories: z.array(z.string()),
+    options: z.array(z.string()),
+    correct_answers: z.array(z.array(z.number().nonnegative())),
+    explanations: z.array(z.string()),
+  })
+  //check if all length array are equal
+  .refine(
+    ({ categories, correct_answers, explanations }) =>
+      compareAllEqual([categories, correct_answers, explanations]),
+    {
+      message:
+        "The length array of categories, correct_answers and explanations must be all equal",
+    },
+  );
+export const directMatchingSubpartSchema = z
+  .object({
+    question: z.string(),
+    categories: z.array(z.string()),
+    options: z.array(z.string()),
+    correct_answers: z.array(z.number().nonnegative()),
+    explanations: z.array(z.string()),
+  })
+  //check if all length array are equal
+  .refine(
+    ({ categories, options, correct_answers, explanations }) =>
+      compareAllEqual([categories, correct_answers, explanations, options]),
+    {
+      message:
+        "The length array of categories, options, correct_answers and explanations must be all equal",
+    },
+  );
+
+export const trueFalseSubpartSchema = z
+  .object({
+    questions: z.array(z.string()),
+    correct_answers: z.array(z.boolean()),
+    explanations: z.array(z.string()),
+  })
+  .refine(
+    ({ questions, correct_answers, explanations }) =>
+      compareAllEqual([questions, correct_answers, explanations]),
+    {
+      message:
+        "The length array of categories, options, correct_answers and explanations must be all equal",
+    },
+  );
+
+export const multipleChoiceSubpartSchema = z
+  .object({
+    question: z.string(),
+    options: z.array(z.string()),
+    correct_answer: z.number().int().nonnegative(),
+    explanations: z.array(z.string()),
+  })
+  .refine(
+    ({ options, correct_answer, explanations }) =>
+      compareAllEqual([explanations, options]) &&
+      correct_answer < options.length,
+    {
+      message:
+        "The lengths array of explanations and options must be equal, and the correct_answer number must be smaller options length",
+    },
+  );
+
+export const selectAllSubpartSchema = z
+  .object({
+    question: z.string(),
+    options: z.array(z.string()),
+    correct_answer: z.array(z.number().int().nonnegative()),
+    explanations: z.array(z.string()),
+  })
+  .refine(
+    ({ options, correct_answer, explanations }) => {
+      const length = options.length;
+      //check if any index answear is greater than options array length
+      for (let i = 0; i < correct_answer.length; i++) {
+        if (correct_answer[i] >= length) return false;
+      }
+      return compareAllEqual([explanations, options]);
+    },
+    {
+      message:
+        "The lengths array of explanations and options must be equal, and the correct_answer number must be smaller options length",
+    },
+  );
+
 export const createQuizSchema = z.object({
-  storyId: z.string(),
+  story_id: z
+    .string({
+      required_error: "story_id is required",
+      invalid_type_error: "story_id must be a ObjectId",
+    })
+    .regex(/^[0-9a-f]{24}$/, { message: "story_id must be a valid ObjectId" }),
   contentCategory: z.string(),
   questionType: z.enum([
     "MULTIPLE_CHOICE",
@@ -10,18 +104,21 @@ export const createQuizSchema = z.object({
     "COMPLEX_MATCHING",
     "SELECT_ALL",
   ]),
-  questionName: z.string(),
-  totalScore: z.number(),
-  subparts: z.array(
-    z.object({
-      question: z.string(),
-      options: z.array(z.string()),
-      correctAnswer: z.array(z.string()),
-      explanation: z.string(),
-    }),
-  ),
+  max_score: z.number().int().nonnegative(),
+  subparts: z.array(z.any()),
 });
-
 export const getQuizzesSchema = z.object({
   storyId: z.string(),
 });
+
+//check if all array length are equals to each other
+function compareAllEqual(values: any[]) {
+  for (let i = 0; i < values.length - 1; i++) {
+    for (let j = i + 1; j < values.length; j++) {
+      const m = values[i].length;
+      const n = values[j].length;
+      if (m !== n) return false;
+    }
+  }
+  return true;
+}
